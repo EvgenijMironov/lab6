44ea05b27a36fc6ca4f1ea49d66b974f5e217db4
Nikita parfenov <n1koss.boy@gmail.com>
2019-12-05 20:06:14 +0500

    Жека плохой я накодил индивидуалку. Проект можно закрыть. Переходим в седьмую лабу

--- lab6.3/Program.cs
+++ lab6.3/Program.cs
@@ -4,135 +4,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab6._3
-{
-    //TODO:
-    // 1. Сделать два подкласса
-    // 2. Сделать виртуальный метод
-    // 3. Сделать поиск
-
-    class Tour
-    {
-        private string location;
-        private int days;
-        private string type;
-        private float priceRoad;
-        private float priceFood;
-        private float priceHotel;
-        private float priceSum;
-        private float priceAvg;
-
-        public Tour(string location, int days, string type, float priceRoad, float priceFood, float priceHotel)
-        {
+namespace lab6._3 {
+    class Tour {
+        protected string vName;
+        protected string location;
+        protected int days;
+        protected float price;
+        public Tour(string vName, string location, int days, float price) {
+            this.vName = vName;
             this.location = location;
             this.days = days;
-            this.type = type;
-            this.priceRoad = priceRoad;
-            this.priceFood = priceFood;
-            this.priceHotel = priceHotel;
-
-            this.priceSum = priceRoad + ((priceFood + priceHotel) * days);
-            this.priceAvg = (float)priceSum / days;
+            this.price = price;
         }
-
-        public Tour() { }
-
-        public string Location
-        {
-            get { return location; }
-            set
-            {
-                if (value != "") location = value;
-                else Console.WriteLine("Input Error");
-            }
+        public string VName {
+            get { return vName; }
         }
-
-        public int Days
-        {
+        public int Days { 
             get { return days; }
-            set
-            {
-                if (value > 0) days = value;
-                else Console.WriteLine("Input Error");
-            }
         }
-
-        public string Type
-        {
-            get { return type; }
-            set
-            {
-                if (value is string && (value == "туристическая" || value == "санаторная")) type = value;
-                else Console.WriteLine("Input Error");
-            }
-        }
-
-        public float PriceRoad
-        {
-            get { return priceRoad; }
-            set
-            {
-                if (value > 0) priceRoad = value;
-                else Console.WriteLine("Input Error");
-            }
-        }
-
-        public float PriceFood
-        {
-            get { return priceFood; }
-            set
-            {
-                if (value > 0) priceFood = value;
-                else Console.WriteLine("Input Error");
-            }
-        }
-
-        public float PriceHotel
-        {
-            get { return priceHotel; }
-            set
-            {
-                if (value > 0) priceHotel = value;
-                else Console.WriteLine("Input Error");
-            }
-        }
-
-        public float PriceSum
-        {
-            get
-            {
-                priceSum = priceRoad + ((priceFood + priceHotel) * days);
-                return priceSum;
-            }
-        }
-
-        public float PriceAvg
-        {
-            get
-            {
-                priceAvg = (float)priceSum / days;
-                return priceAvg;
-            }
+        public virtual void getInfo() {
+            Console.WriteLine($"Название: {vName}\n" +
+                $"Страна: {location}\n" +
+                $"Срок: {days} дней\n" +
+                $"Цена: {price}");
         }
     }
 
-    class Program
-    {
-        static void search(List<Tour> temp, string name) {
-            foreach (Tour t in temp) { }
+    class TravelV : Tour {
+        protected string hotelName;
+        public TravelV(string vName, string location, int days, float price, string hotelName) : base(vName, location, days, price) {
+            this.hotelName = hotelName;
+        }
+        public override void getInfo() {
+            Console.WriteLine($"Название путевки: {vName}\n" +
+                $"Страна: {location}\n" +
+                $"Срок: {days} дней\n" +
+                $"Цена: {price}\n" +
+                $"Название отеля: {hotelName}");
+        }
+    }
+
+    class SanatoriumV : Tour {
+        protected string organisationName;
+        public SanatoriumV(string vName, string location, int days, float price, string organisationName) : base(vName, location, days, price) {
+            this.organisationName = organisationName;
+        }
+        public override void getInfo() {
+            Console.WriteLine($"Название путевки: {vName}\n" +
+                $"Страна: {location}\n" +
+                $"Срок: {days} дней\n" +
+                $"Цена: {price}\n" +
+                $"Название организации: {organisationName}");
+        }
+    }
+    class Program {
+        static void search(List<Tour> temp, string vName, int days) {
+            foreach (Tour t in temp)
+                if (vName == t.VName && days == t.Days) t.getInfo();
         }
         static void Main(string[] args) {
             List<Tour> t = new List<Tour>();
             Console.Write("Введите количество путевок: "); int n = Convert.ToInt16(Console.ReadLine());
             for (int i = 0; i < n; i++) {
-                Console.Write("Куда едем?: "); string Location = Console.ReadLine();
-                Console.Write("На сколько дней?: "); int Days = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Туристическая или санаторная путевка?: "); string Type = Console.ReadLine();
-                Console.Write("Цена дороги: "); float PriceRoad = Convert.ToSingle(Console.ReadLine());
-                Console.Write("Цена еды: "); float PriceFood = Convert.ToSingle(Console.ReadLine());
-                Console.Write("Цена проживания: "); float PriceHotel = Convert.ToSingle(Console.ReadLine());
-                t.Add(new Tour(Location, Days, Type, PriceRoad, PriceFood, PriceHotel));
+                Console.Write("Туристическая или санаторная?(т/с) "); string type = Console.ReadLine();
+                if (type == "т") {
+                    Console.Write("Название путевки: "); string VName = Console.ReadLine();
+                    Console.Write("Куда едем?: "); string Location = Console.ReadLine();
+                    Console.Write("На сколько дней?: "); int Days = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Цена: "); float Price = Convert.ToSingle(Console.ReadLine());
+                    Console.Write("Название отеля: "); string HotelName = Console.ReadLine();
+                    t.Add(new TravelV(VName, Location, Days, Price, HotelName));
+                } else if (type == "с") {
+                    Console.Write("Название путевки: "); string VName = Console.ReadLine();
+                    Console.Write("Куда едем?: "); string Location = Console.ReadLine();
+                    Console.Write("На сколько дней?: "); int Days = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Цена: "); float Price = Convert.ToSingle(Console.ReadLine());
+                    Console.Write("Название организации: "); string OrganisationName = Console.ReadLine();
+                    t.Add(new SanatoriumV(VName, Location, Days, Price, OrganisationName));
+                } else { Console.WriteLine("ОШИБКА!!!!!!"); i--; }
             }
+            Console.WriteLine("ПОИСК");
+            Console.Write("Введите название путевки: "); string vName = Console.ReadLine();
+            Console.Write("Введите количество дней: "); int days = Convert.ToInt32(Console.ReadLine());
+            search(t, vName, days);
             Console.ReadKey();
         }
     }
